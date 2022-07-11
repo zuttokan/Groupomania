e068f97930d0c66fd2a905c49e10f8a05da38c09
zuttokan <zutto.perez@gmail.com>
2022-07-11 14:33:08 +0100

    front

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostListComponent } from './post-list/post-list.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
 import { Post } from './models/post.model';
 import { NewPostComponent } from './new-post/new-post.component';
 
 import { SinglePostComponent } from './single-post/single-post.component';
+import { LoginComponent } from './auth/components/login/login.component';
 
 const routes: Routes = [
   {
@@ -23,7 +23,7 @@ const routes: Routes = [
 
   {
     path: '',
-    component: LandingPageComponent,
+    component: LoginComponent,
   },
 
   {
